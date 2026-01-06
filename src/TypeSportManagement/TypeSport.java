c5a3e62e1c76d2e1bc694ae3600991c0da51a308
matteodenee <matteo.denee@gmail.com>
2026-01-06 17:05:25 +0100

    fix de licenceFrame

--- src/TypeSportManagement/TypeSport.java
+++ src/TypeSportManagement/TypeSport.java
@@ -2,6 +2,22 @@ package TypeSportManagement;
 
 public class TypeSport {
 
+    private final int id;
+    private final String name;
+
+    public TypeSport(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() { return id; }
+    public String getName() { return name; }
+
+    @Override
+    public String toString() {
+        return name; // pour afficher dans la ComboBox
+    }
+
     public static TypeSport valueOf(String string) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'valueOf'");
@@ -10,6 +26,11 @@ public class TypeSport {
     public String getNom() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'getNom'");
+    }
+
+    public static Object values() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'values'");
     }
     
 }
